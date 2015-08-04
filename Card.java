c402998459062f9ddd0a72a156667e05acc3f51c
extendsLcc <lucas.lcc@hotmail.com>
2015-08-04 18:21:22 -0300

    deck and card

--- Card.java
+++ Card.java
@@ -8,12 +8,80 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Card extends Actor
 {
-    /**
-     * Act - do whatever the Card wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+    
+    private int simbol;
+    private int naipe;
+    
+    private boolean flip = false;
+    private GreenfootImage cardFace;
+    
+    private static final GreenfootImage background = new GreenfootImage( "background.png" );
+    private static final GreenfootImage allFaces = new GreenfootImage( "classic-playing-cards.png" );
+    
+    public Card( int simbol, int naipe ){
+    
+        this.simbol = simbol;
+        this.naipe = naipe;
+        
+        setImage( background );
+        cardFace = Sprite.getSprite( allFaces, 73, 98, simbol + naipe * 10, 73, 98 );
+        
+    }
+    
+    public boolean isFliped(){
+    
+        return flip;
+        
+    }
+    
+    public void flip(){
+    
+        if( flip ){
+        
+            setImage( background );
+            
+        }else
+            setImage( cardFace );
+            
+        flip = !flip;
+    
+    }
+    
+    private int[] simbolPower = { 2, 1, 0, 9, 8, 7, 6, 5, 4, 3 };
+    private int[] naipePower = { 3, 1, 2, 0 };
+    
+    public int getCardPower( Card manilha ){
+        
+        int tmp = simbol - 1 == -1 ? 9 : simbol - 1;
+        
+        if( manilha.simbol == tmp ){
+        
+            for( int i = 0; i < naipePower.length; i ++){
+            
+                return -( i + 1 );
+            
+            }
+            
+        }
+        
+        for( int i = 0; i < simbolPower.length; i ++ ){
+        
+            if( simbol == simbolPower[i] )
+                return i;
+            
+        }
+    
+        return 99;// erro
+        
+    }
+    
+    public void atualize(){
+    
+        World w = getWorld();
+        int x = getX(), y = getY();
+        w.removeObject( this );
+        w.addObject( this, x, y );
+        
+    }
+    
 }
--- Deck.java
+++ Deck.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
 
 /**
  * Write a description of class Deck here.
@@ -8,12 +9,88 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Deck extends Actor
 {
-    /**
-     * Act - do whatever the Deck wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+    
+    ArrayList < Card > deck =  new ArrayList < Card >();
+    
+    public Deck(){
+    
+        //getImage().clear();
+    
+    }
+    
+    public void generateDeck(){
+
+        int deck[] = new int[10];
+        int naipes[][] = new int[10][4];
+        World world = getWorld();
+
+        while( this.deck.size() < 40){
+
+            int random = Greenfoot.getRandomNumber( 10 );
+
+            if ( deck[random] < 4 ){
+
+                while( true ){
+                    
+                    int rnd = Greenfoot.getRandomNumber( 4 );
+                    
+                    if ( naipes[random][rnd] < 1 ){
+                    
+                        deck[random] ++;
+                        naipes[random][rnd] ++;
+                        Card c = new Card( random, rnd );
+                        world.addObject( c, 200, 200 );
+                        this.deck.add( c );
+                        break;
+                        
+                    }
+                    
+                }
+
+            }
+            
+        }
+        
+    }
+    
+    public void shuffleDeck( /*int x, int y*/ ){
+        
+        int x = 560;
+        int y = 200;
+    
+        ArrayList < Card > l = new ArrayList < Card >();
+        World w = getWorld();
+        int cards = 0;
+        
+        for( Card c : deck ){
+        
+            w.removeObject( c );
+            if( c.isFliped() )c.flip();
+            
+        }
+        
+        while( cards < 40 ){
+        
+            int i  = Greenfoot.getRandomNumber( deck.size() );
+            
+            if( ! l.contains( deck.get( i ) ) ){
+            
+                Card c = deck.get( i );
+                l.add( c );
+                deck.remove( c );
+                w.addObject( c, x - cards, y - ( cards * 2 ) );
+                cards ++;
+                Greenfoot.delay( 1 );
+                
+            }
+        
+        }
+        
+        deck.clear(); //!! só pra ter certeza
+        deck.addAll( l );
+        //vira().flip();
+        //vira().dragTo( 0, (int) Math.round( polarX( vira().getX(), 50, 135 ) ), (int) Math.round( polarY( vira().getY(), 50, 135 ) ) );
+        
+    }
+    
 }
