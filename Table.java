f6d462adea99b57ff097f245091a1333e24919fb
extendsLcc <lucas.lcc@hotmail.com>
2015-08-04 16:08:01 -0300

    origin

---
+++ Table.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Table here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Table extends World
+{
+
+    /**
+     * Constructor for objects of class Table.
+     * 
+     */
+    public Table()
+    {    
+        
+        super(600, 400, 1); 
+        
+    }
+}
